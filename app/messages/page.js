5637afa3f7af79674501d7e997fd9caa8f6809d9
Progress with the component <diedemiguel@gmail.com>
2024-05-05 18:10:50 +0200

    Refactor fetch URLs to use double quotes instead of single quotes

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,11 +1,10 @@
-import Messages from '@/components/messages';
+import Messages from "@/components/messages";
+
+// export const revalidate = 5;
+export const dynamic = "force-dynamic"; // equals to cache = 'no-store' on the fetch object.
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
+  const response = await fetch("http://localhost:8080/messages");
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
